7a2ae61a4e816f0c5adbfa55afaebf7550735417
LisaVanderElst <LisaVanderelst19@gmail.com>
2025-01-14 14:30:03 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,37 @@
+let graden = 0
+basic.forever(function () {
+    graden = input.compassHeading()
+    if (graden < 45 || graden > 315) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (graden < 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (graden < 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
